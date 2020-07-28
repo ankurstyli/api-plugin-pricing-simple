6309c63f1b7387c326f8e3d135c763824cbc092f
Ankur Pata <ankur@stylishop.com>
2020-07-28 12:15:06 +0400

    Pricing schema changes for multi shop support

--- src/xforms/xformProductVariantPricing.js
+++ src/xforms/xformProductVariantPricing.js
@@ -4,28 +4,55 @@ import getDisplayPrice from "../util/getDisplayPrice.js";
 
 export default async (node, context) => {
   const { Shops } = context.collections;
-  const shop = await Shops.findOne({ _id: node.shopId }, { projection: { currency: 1 } });
-  const currencyDefinition = getCurrencyDefinitionByCode(shop.currency);
-
   let pricing = {};
-  const { price } = node;
-  if (typeof price === "object") {
-    pricing = {
-      compareAtPrice: null,
-      displayPrice: getDisplayPrice(price.min, price.max, currencyDefinition),
-      maxPrice: price.max,
-      minPrice: price.min,
-      price: null
-    };
-  } else {
-    pricing = {
-      compareAtPrice: node.compareAtPrice || 0,
-      displayPrice: accounting.formatMoney(price, currencyDefinition.symbol),
-      maxPrice: price || 0,
-      minPrice: price || 0,
-      price: price || 0
-    };
-  }
+  if(typeof node.shopId == 'object'){
+    console.log(node.shopId, '*******node.shopId@xformProductVariantPricing**********');
+    for(const subShopId of node.shopId){
+      console.log('*******@subShop******', subShopId)
+      const shop = await Shops.findOne({_id: subShopId}, {projection: {currency: 1}});
+      const currencyDefinition = getCurrencyDefinitionByCode(shop.currency);
 
+      const {price} = node;
+      if (typeof price === "object") {
+        pricing[subShopId] = {
+          compareAtPrice: null,
+          displayPrice: getDisplayPrice(price.min, price.max, currencyDefinition),
+          maxPrice: price.max,
+          minPrice: price.min,
+          price: null
+        };
+      } else {
+        pricing[subShopId] = {
+          compareAtPrice: node.compareAtPrice || 0,
+          displayPrice: accounting.formatMoney(price, currencyDefinition.symbol),
+          maxPrice: price || 0,
+          minPrice: price || 0,
+          price: price || 0
+        };
+      }
+    }
+  }else {
+    const shop = await Shops.findOne({_id: node.shopId}, {projection: {currency: 1}});
+    const currencyDefinition = getCurrencyDefinitionByCode(shop.currency);
+
+    const {price} = node;
+    if (typeof price === "object") {
+      pricing = {
+        compareAtPrice: null,
+        displayPrice: getDisplayPrice(price.min, price.max, currencyDefinition),
+        maxPrice: price.max,
+        minPrice: price.min,
+        price: null
+      };
+    } else {
+      pricing = {
+        compareAtPrice: node.compareAtPrice || 0,
+        displayPrice: accounting.formatMoney(price, currencyDefinition.symbol),
+        maxPrice: price || 0,
+        minPrice: price || 0,
+        price: price || 0
+      };
+    }
+  }
   return pricing;
 };
